076b6b60ff50079b29b8c67f0e98ce3d2bfeffd7
martinschmelzer <schmelzer.martin@gmx.de>
2018-03-15 13:59:43 +0100

    Update hideOutput.js
    Minor fix on copy button class

--- js/hideOutput.js
+++ js/hideOutput.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
   // Copy Button
   $chunks = $('pre.sourceCode > code.sourceCode');
   $chunks.each(function(i, val) {
-    $(this).prepend("<button type=\"button\" class=\"copy\"><i class=\"fa fa-copy fa-2x\"></i></div>").click(function() {
+    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
       var $temp = $("<textarea>");
       $("body").append($temp);
       var content = $(this).clone().children("button").remove().end().text();
