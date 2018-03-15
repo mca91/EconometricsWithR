1137790e82ce1588b8f50fc0c091c73c7bfea125
Emwikts1970 <martin.emwikts@googlemail.com>
2018-03-15 15:18:32 +0100

    update for copy functionalities

--- docs/js/hideOutput.js
+++ docs/js/hideOutput.js
@@ -28,6 +28,22 @@ $(document).ready(function() {
     }
       $(this).parent().find('code, img').slideToggle('fast', 'swing');
   });
+  
+  
+  // Copy Button
+  $chunks = $('pre.sourceCode > code.sourceCode');
+  $chunks.each(function(i, val) {
+    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
+      var $temp = $("<textarea>");
+      $("body").append($temp);
+      var content = $(this).clone().children("button").remove().end().text();
+      $temp.val(content).select();
+      document.execCommand("copy");
+      $temp.remove();
+    });
+  });
+  
+  
 });
 
 
