403d668b43726004da2a707b0f591f9c39680b9a
mca91 <martin.emwikts@googlemail.com>
2019-08-30 10:56:20 +0200

    added download button to code chunks

--- js/hideOutput.js
+++ js/hideOutput.js
@@ -28,24 +28,35 @@ $(document).ready(function() {
 //     }
 //       $(this).parent().find('code, img').slideToggle('fast', 'swing');
 //   });
-  
+
   // Copy Button
   $chunks = $('pre.sourceCode > code.sourceCode');
-  $chunks.each(function(i, val) {
-    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
-      var $temp = $("<textarea>");
-      $("body").append($temp);
-      var content = $(this).clone().children("button").remove().end().text();
-      $temp.val(content).select();
-      document.execCommand("copy");
-      $temp.remove();
+  
+    $chunks.each(function(i, val) {
+    $(this).prepend("<button class=\"download\"><i class=\"fa fa-download fa-2x\"></i></button>").click(function() {
+        var $temp = $('<a />');
+        $("body").append($temp);
+        var content = $(this).clone().children("button").remove().end().text();
+        $temp.attr({
+              download: $(this).closest('div').attr('id') +'.R', 
+              href: "data:text/plain;charset=utf-8," + encodeURIComponent(content)
+        })[0].click();
+        $temp.remove();
     });
   });
   
+//  $chunks.each(function(i, val) {
+//  
+//    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
+//      var $temp = $("<textarea>");
+//      $("body").append($temp);
+//      var content = $(this).clone().children("button").remove().end().text();
+//      $temp.val(content).select();
+//      document.execCommand("copy");
+//      $temp.remove();
+//    });
+//  });
   
+
 });
-
-
-
-
 
